916f0568479bac06bbe523d558dd27f474b1248b
Serkalem Hailu <serkalembekele081@gmail.com>
2022-04-16 17:26:42 -0500

    orderUpdate

--- src/main/java/com/example/waaonlineminimarketbackend/service/Implementation/OrderStatusServiceImpl.java
+++ src/main/java/com/example/waaonlineminimarketbackend/service/Implementation/OrderStatusServiceImpl.java
@@ -1,35 +1,45 @@
 package com.example.waaonlineminimarketbackend.service.Implementation;
 
-import com.example.waaonlineminimarketbackend.entity.Item;
+
+import com.example.waaonlineminimarketbackend.entity.OrderStatus;
+import com.example.waaonlineminimarketbackend.repository.OrderStatusRepository;
 import com.example.waaonlineminimarketbackend.service.OrderStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OrderStatusServiceImpl implements OrderStatusService {
-    @Override
-    public void saveOrderStatus() {
 
+    @Autowired
+    OrderStatusRepository orderStatusRepository;
+
+    @Override
+    public void saveOrderStatus(OrderStatus os) {
+      orderStatusRepository.save(os);
     }
 
     @Override
-    public List<Item> getAllOrderStatus() {
-        return null;
+    public List<OrderStatus> getAllOrderStatus() {
+        return orderStatusRepository.findAll();
     }
 
     @Override
-    public Item getOrderStatusById(long id) {
-        return null;
+    public OrderStatus getOrderStatusById(long id) {
+        return  orderStatusRepository.getById(id);
     }
 
     @Override
     public void deleteOrderStatusById(long id) {
+        orderStatusRepository.deleteById(id);
 
     }
 
     @Override
-    public void UpdateOrderStatusById(long id) {
-
+    public void UpdateOrderStatusById(long id, OrderStatus os) {
+         var orderStatus = orderStatusRepository.getById(id);
+         orderStatus.setId(os.getId());
+         orderStatus.setName(os.getName());
     }
 }
--- src/main/java/com/example/waaonlineminimarketbackend/service/Implementation/ReviewServiceImpl.java
+++ src/main/java/com/example/waaonlineminimarketbackend/service/Implementation/ReviewServiceImpl.java
@@ -1,35 +1,44 @@
 package com.example.waaonlineminimarketbackend.service.Implementation;
 
 import com.example.waaonlineminimarketbackend.entity.Review;
+import com.example.waaonlineminimarketbackend.repository.ReviewRepository;
 import com.example.waaonlineminimarketbackend.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
-    @Override
-    public void saveReview() {
+    @Autowired
+    ReviewRepository reviewRepository;
 
+    @Override
+    public void saveReview(Review review) {
+     reviewRepository.save(review);
     }
 
     @Override
     public List<Review> getAllReview() {
-        return null;
+        return reviewRepository.findAll();
     }
 
     @Override
     public Review getReviewById(long id) {
-        return null;
+        return reviewRepository.getById(id);
     }
 
     @Override
     public void deleteReviewById(long id) {
+         reviewRepository.deleteById(id);
 
     }
 
     @Override
-    public void UpdateReviewById(long id) {
+    public void UpdateReviewById(long id, Review rv) {
+        var review = reviewRepository.getById(id);
+        review.setReviewId(rv.getReviewId());
+        review.setName(rv.getName());
 
     }
 }
