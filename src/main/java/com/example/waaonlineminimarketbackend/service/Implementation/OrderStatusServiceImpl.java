791c7595bc3e033843b1ba9bb1e4dc44b7bdf173
Serkalem Hailu <serkalembekele081@gmail.com>
2022-04-16 15:27:27 -0500

    initial
    service

---
+++ src/main/java/com/example/waaonlineminimarketbackend/service/Implementation/OrderStatusServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.waaonlineminimarketbackend.service.Implementation;
+
+import com.example.waaonlineminimarketbackend.entity.Item;
+import com.example.waaonlineminimarketbackend.service.OrderStatusService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderStatusServiceImpl implements OrderStatusService {
+    @Override
+    public void saveOrderStatus() {
+
+    }
+
+    @Override
+    public List<Item> getAllOrderStatus() {
+        return null;
+    }
+
+    @Override
+    public Item getOrderStatusById(long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrderStatusById(long id) {
+
+    }
+
+    @Override
+    public void UpdateOrderStatusById(long id) {
+
+    }
+}
--- src/main/java/com/example/waaonlineminimarketbackend/service/OrderService.java
+++ src/main/java/com/example/waaonlineminimarketbackend/service/OrderService.java
@@ -1,4 +1,14 @@
 package com.example.waaonlineminimarketbackend.service;
 
+import com.example.waaonlineminimarketbackend.entity.Item;
+
+import java.util.List;
+
 public interface OrderService {
+
+    public void saveOrder();
+    public List<Item> getAllOrder();
+    public Item getOrderById(long id);
+    public void deleteOrderById(long id);
+    public void UpdateOrderById(long id);
 }
