452acbc17b638de8a13cc86f9285d30ca0351f04
Abenezer <abenitrust@gmail.com>
2022-04-19 18:29:16 -0500

    Helper method to find authenticated User

--- src/main/java/com/example/waaonlineminimarketbackend/util/AuthenticatedUser.java
+++ src/main/java/com/example/waaonlineminimarketbackend/util/AuthenticatedUser.java
@@ -2,7 +2,6 @@ package com.example.waaonlineminimarketbackend.util;
 
 import com.example.waaonlineminimarketbackend.entity.User;
 import com.example.waaonlineminimarketbackend.repository.UserRepository;
-import com.example.waaonlineminimarketbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,10 +14,9 @@ public class AuthenticatedUser {
     UserRepository userRepository;
 
     public User getCurrentUser() {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        UserDetails userDetail =(UserDetails) auth.getDetails();
-//        User user = userRepository.findByEmail(userDetail.getUsername());
-        User user = userRepository.getById(18L);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetail =(UserDetails) auth.getPrincipal();
+        User user = userRepository.findByEmail(userDetail.getUsername());
         return user;
     }
 }
