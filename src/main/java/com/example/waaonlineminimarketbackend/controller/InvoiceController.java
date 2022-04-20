43b7d5c7aa4704645d14ba2c8beb115a33cfb26e
Serkalem Hailu <serkalembekele081@gmail.com>
2022-04-19 21:08:07 -0500

    receipt pdf

---
+++ src/main/java/com/example/waaonlineminimarketbackend/controller/InvoiceController.java
@@ -0,0 +1,57 @@
+package com.example.waaonlineminimarketbackend.controller;
+
+import java.io.IOException;
+import java.text.DateFormat;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.waaonlineminimarketbackend.entity.Order;
+import com.example.waaonlineminimarketbackend.entity.dto.output.OrderOutputDto;
+import com.example.waaonlineminimarketbackend.entity.dto.output.UserOutputDto;
+import com.example.waaonlineminimarketbackend.service.OrderService;
+import com.example.waaonlineminimarketbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.lowagie.text.DocumentException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/invoice")
+public class InvoiceController {
+
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping("/export")
+    public void  exportToPDF(HttpServletResponse response)  throws   DocumentException, IOException {
+
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".pdf";
+
+        response.setHeader(headerKey, headerValue);
+
+        List<OrderOutputDto> listOfOrder = orderService.getAllOrder();
+
+        UserPDFExporter exporter = new UserPDFExporter(listOfOrder);
+
+        exporter.export(response);
+
+    }
+
+}
